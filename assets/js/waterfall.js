f2138aa69fb30792d393924bb0a125a9cb59ed2c
frostace <frostace0723@gmail.com>
2020-02-02 21:59:02 -0500

    release2.4
    1. prototype of waterfall finished
    2. fixed map-marker conflict issue

--- assets/js/waterfall.js
+++ assets/js/waterfall.js
@@ -1,69 +1,87 @@
-let openModalButtons;
-let closeModalButtons;
-let overlay;
-let modal;
-
 // must register these elements after DOM is ready
 document.addEventListener('DOMContentLoaded', function(event) {
     //the event occurred
-    openModalButtons = document.querySelectorAll('[data-modal-target]');
-    closeModalButtons = document.querySelectorAll('[data-close-button]');
-    overlay = document.getElementById('overlay');
-    modal = document.getElementById('modal');
+    // openModalButtons = document.querySelectorAll('[data-modal-target]');
+    // closeModalButtons = document.querySelectorAll('[data-close-button]');
+    // overlay = document.getElementById('overlay');
+    // modal = document.getElementById('modal');
 
-    for (var i = 0; i < openModalButtons.length; i++) {
-        obj = openModalButtons[i];
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    var brickList = document.querySelectorAll('[data-waterfall]');
+    console.log(brickList);
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+    }
+
+
+    for (var i = 0; i < brickList.length; i++) {
+        obj = brickList[i];
         obj.addEventListener('click', () => {
             // const modal = document.querySelector(obj.dataset.modalTarget);
             console.log(obj);
             console.log(obj.id + "clicked");
-            openModal(modal, obj.id);
+            modal.style.display = "block";
+            displayProject(obj.id);
         })
     }
 
-    console.log(openModalButtons);
+    // console.log(openModalButtons);
 
-    overlay.addEventListener('click', () => {
-        const modals = document.querySelectorAll('.modal.active');
-        modals.forEach(modal => {
-            closeModal(modal);
-        })
-    })
+    // overlay.addEventListener('click', () => {
+    //     const modals = document.querySelectorAll('.modal.active');
+    //     modals.forEach(modal => {
+    //         closeModal(modal);
+    //     })
+    // })
 
-    closeModalButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            const modal = button.closest('.modal');
-            closeModal(modal);
-        })
-    })
+    // closeModalButtons.forEach(button => {
+    //     button.addEventListener('click', () => {
+    //         const modal = button.closest('.modal');
+    //         closeModal(modal);
+    //     })
+    // })
 })
 
-function openbala(idNum) {
-    if (modal == null) return;
-    modal.classList.add('active');
-    overlay.classList.add('active');
-    displayProject(idNum);
-    alert(idNum);
-}
 
-function openModal(modal, projectNum) {
-    console.log("opening modal:", modal, projectNum);
-    if (modal == null) return;
-    modal.classList.add('active');
-    overlay.classList.add('active');
-    displayProject(projectNum);
-}
+// function openModal(modal, projectNum) {
+//     console.log("opening modal:", modal, projectNum);
+//     if (modal == null) return;
+//     modal.classList.add('active');
+//     overlay.classList.add('active');
+//     displayProject(projectNum);
+// }
 
-function closeModal(modal) {
-    if (modal == null) return;
-    // remove all children in modal-body
-    var scrollWindow = document.getElementById("modal-body");
-    while (scrollWindow.hasChildNodes()) {  
-        scrollWindow.removeChild(scrollWindow.firstChild);
-    }
-    modal.classList.remove('active');
-    overlay.classList.remove('active');
-}
+// function closeModal(modal) {
+//     if (modal == null) return;
+//     // remove all children in modal-body
+//     var scrollWindow = document.getElementById("modal-body");
+//     while (scrollWindow.hasChildNodes()) {  
+//         scrollWindow.removeChild(scrollWindow.firstChild);
+//     }
+//     modal.classList.remove('active');
+//     overlay.classList.remove('active');
+// }
 
 let iterEnd = false;
 function displayProject(projectNum, flag=false) {
