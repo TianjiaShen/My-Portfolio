fe07cb0268a7e34d161bb1032bb4413203e08848
frostace <frostace0723@gmail.com>
2020-02-04 23:02:53 -0500

    release3.3
    1. Disabled overlay onclick

--- assets/js/waterfall.js
+++ assets/js/waterfall.js
@@ -37,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
     if (event.target == modal) {
-        closeModal();
+        // closeModal();
     }
     }
 
